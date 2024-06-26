02d35b864192e3f806994cc098ac2514054d6cc3
GAJAM JYOTHSNA <96329766+Gajamjyothsna@users.noreply.github.com>
2024-06-26 22:13:29 +0530

    Implemented joystick with basic player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     #region Private Variables
     [SerializeField] private Animator playerAnimatorController;
+    [SerializeField] private VariableJoystick joystick;
+    [SerializeField] private Canvas inputCanvas;
+    private bool isJoystick;
     #endregion
     // Start is called before the first frame update
     void Start()
     {
-        
+        EnableJoyStickInput();
+    }
+
+    private void EnableJoyStickInput()
+    {
+        isJoystick = true;
+        inputCanvas.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       /* 
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(horizontal, 0, vertical) * 1 * Time.deltaTime;
@@ -28,6 +38,20 @@ public class PlayerController : MonoBehaviour
         {
             playerAnimatorController.SetFloat("moveAmount", 0);
         }
-        transform.Translate(movement);
+        transform.Translate(movement);*/
+
+        if(isJoystick)
+        {
+            Vector3 movement = new Vector3(joystick.Direction.x, 0, joystick.Direction.y) * 1 * Time.deltaTime;
+            if (movement.magnitude > 0)
+            {
+                playerAnimatorController.SetFloat("moveAmount", 1);
+            }
+            if (movement.magnitude == 0)
+            {
+                playerAnimatorController.SetFloat("moveAmount", 0);
+            }
+            transform.Translate(movement);
+        }
     }
 }
